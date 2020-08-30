0807c2d101b6062c71f98bd7da6842d1c3415758
561565 <1006976098@qq.com>
2020-08-31 01:46:38 +0800

    修改了path获取方式 Global.href

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Tab.less";
-import { Global } from '../../models/Global';
+import { Global } from "../../models/Global";
 
-export interface TabProps { }
+export interface TabProps {}
 
 export interface TabState {
     selectedIndex: number;
@@ -16,71 +16,80 @@ export interface TabState {
 
 export class Tab extends React.Component<TabProps, TabState> {
     state: TabState = {
-        selectedIndex: -1,  //-1不显示，0~n为index
+        selectedIndex: -1, //-1不显示，0~n为index
         icontList: [
             {
-                img: ("/src/components/Tab/assets/home.png"),
-                imgSelected: ("/src/components/Tab/assets/home_sel.png"),
+                img: "/src/components/Tab/assets/home.png",
+                imgSelected: "/src/components/Tab/assets/home_sel.png",
                 name: "主页",
-                path: 'home'
+                path: "home",
             },
             {
-                img: ("/src/components/Tab/assets/category.png"),
-                imgSelected: ("/src/components/Tab/assets/category_sel.png"),
+                img: "/src/components/Tab/assets/category.png",
+                imgSelected: "/src/components/Tab/assets/category_sel.png",
                 name: "分类",
-                path: 'category'
+                path: "category",
             },
             {
-                img: ("/src/components/Tab/assets/discover.png"),
-                imgSelected: ("/src/components/Tab/assets/discover_sel.png"),
+                img: "/src/components/Tab/assets/discover.png",
+                imgSelected: "/src/components/Tab/assets/discover_sel.png",
                 name: "发现",
-                path: 'discover'
+                path: "discover",
             },
             {
-                img: ("/src/components/Tab/assets/cart.png"),
-                imgSelected: ("/src/components/Tab/assets/cart_sel.png"),
+                img: "/src/components/Tab/assets/cart.png",
+                imgSelected: "/src/components/Tab/assets/cart_sel.png",
                 name: "购物车",
-                path: 'cart'
+                path: "cart",
             },
             {
-                img: ("/src/components/Tab/assets/user.png"),
-                imgSelected: ("/src/components/Tab/assets/user_sel.png"),
+                img: "/src/components/Tab/assets/user.png",
+                imgSelected: "/src/components/Tab/assets/user_sel.png",
                 name: "我的",
-                path: 'mine'
+                path: "mine",
             },
         ],
     };
 
     componentDidMount() {
-        this.getPathIndex()
+        this.getPathIndex();
         Global.history.listen(() => {
-            this.getPathIndex()
-        })
+            this.getPathIndex();
+        });
     }
 
     getPathIndex() {
         // let path = Global.history.location.pathname.split('/')[1]
-        let path = Global.history.location.pathname.replace('/', '')
-        this.state.selectedIndex = this.state.icontList.findIndex((v) => (v.path === path))
-        this.forceUpdate()
-
+        let path = Global.href.replace("#/", "");
+        this.state.selectedIndex = this.state.icontList.findIndex(
+            (v) => v.path === path
+        );
+        this.forceUpdate();
     }
 
     render() {
         return (
-            <div className="Tab" style={{ display: this.state.selectedIndex === -1 ? 'none' : '' }}>
+            <div
+                className="Tab"
+                style={{
+                    display: this.state.selectedIndex === -1 ? "none" : "",
+                }}
+            >
                 {this.state.icontList.map((v, i) => (
-                    <div key={i}
+                    <div
+                        key={i}
                         onClick={() => {
-                            Global.history.push('/' + this.state.icontList[i].path)
+                            Global.history.push(
+                                "/" + this.state.icontList[i].path
+                            );
                         }}
                     >
                         <div className="img">
                             {this.state.selectedIndex === i ? (
                                 <img src={v.imgSelected} alt="" />
                             ) : (
-                                    <img src={v.img} alt="" />
-                                )}
+                                <img src={v.img} alt="" />
+                            )}
                         </div>
                         <div className="name">{v.name}</div>
                     </div>
