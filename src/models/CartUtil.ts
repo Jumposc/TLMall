ccb3e8fdfedb4e2e55e5cfd4007dae3d226f63e5
561565 <1006976098@qq.com>
2020-09-23 22:13:26 +0800

    修改CartUtil

--- src/models/CartUtil.ts
+++ src/models/CartUtil.ts
@@ -1,7 +1,8 @@
 import { OrderUtil } from './OrderUtil'
+import { ProductUtil, ProductData } from './ProductUtil'
 
 export interface CartItem {
-    productId: string,
+    id: string,
     amount: number,
     spec: string,
     isSelected?: boolean
@@ -15,13 +16,22 @@ export interface DisplayCartItem extends CartItem {
     maxAmount: number,
 }
 
+export interface CartProductInfo {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: number,
+    freight: number,
+    maxAmount: number,
+
+}
 
 // 购物车
 export class CartUtil {
     static _data: { [userId: string]: CartItem[] } = {
         '11111': [
             {
-                productId: "11",
+                id: "11",
                 amount: 5,
                 spec: "绿色",
                 isSelected: true,
@@ -29,61 +39,53 @@ export class CartUtil {
         ],
     }
 
-    static pList = [
-        {
-            productId: '11',
-            name: '111',
-            imageUrl: '2222',
-            price: 1000,
-            freight: 0,
-            maxAmount: 10,
-        },
-        {
-            productId: '22',
-            name: '555',
-            imageUrl: '666',
-            price: 1000,
-            freight: 0,
-            maxAmount: 10,
-        }
-    ]
-
     static async getList(userId: string): Promise<DisplayCartItem[]> {
-        let productIdList = this._data[userId].map(v => (v.productId))
-        // let productList = await xxx(productIdList)
-        let productList = this.pList
-
+        //分离出productIdList
+        let productIdList = this._data[userId].map(v => (v.id))
+        let products: (ProductData | undefined)[] = await ProductUtil.getProducts(productIdList)
+        // 去除数组中的undefined
+        let productList: ProductData[] = products.filter(v => v !== undefined) as ProductData[]
         return productList.map(v => {
-            let data: CartItem = this._data[userId].find(v1 => (v1.productId === v.productId))!
+            let data: CartItem = this._data[userId].find(v1 => (v1.id === v.id))!
             return ({
                 ...data,
                 name: v.name || "",
                 imageUrl: v.imageUrl || "",
-                price: v.price || 0,
-                freight: v.freight || 0,
-                maxAmount: v.maxAmount || 10,
+                price: v.detail.price || 0,
+                freight: v.detail.freight || 0,
+                maxAmount: v.detail.maxAmount || 10,
             })
         })
     }
 
     /** 往购物车中增加商品 */
     static async add(userId: string, items: CartItem[]): Promise<void> {
-        this._data[userId].push(...items)
+        if (this._data[userId] === undefined) {
+            this._data[userId] = items
+        }
+        else {
+            this._data[userId].push(...items)
+        }
     }
 
     /** 下单 */
     static async placeOrder(userId: string): Promise<{ orderId: string }> {
         let productList = this._data[userId].filter(v => v.isSelected === true)
+
         //生成订单 获取订单id
         let orderId: string = (await OrderUtil.add(userId, productList)).orderId
+
+        // 列表中去除已下单的商品
         this._data[userId] = this._data[userId].filter(v => v.isSelected !== true)
         return { orderId: orderId }
     }
 
     /** 直接购买 */
     static async buy(userId: string, items: CartItem[]): Promise<{ orderId: string }> {
+
         //生成订单 获取订单id
         let orderId: string = (await OrderUtil.add(userId, items)).orderId
+
         return { orderId: orderId }
     }
 
